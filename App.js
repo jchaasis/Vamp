c68cfa197967adf9e8ca160320f5a4ea20ecbc8a
jchaasis <jchaasis@gmail.com>
2017-10-12 11:18:02 -0400

    Add footer bar, set initial app width

--- App.js
+++ App.js
@@ -10,10 +10,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-
           <h1 className="App-title">Vamp</h1>
         </header>
+
         <EventForm />
+
+
+        <footer className="App-footer">
+        </footer>
 
       </div>
     );
