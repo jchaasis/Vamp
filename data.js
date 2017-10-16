9c97e2e8e8b21bd7597fb112fdde6d524c7008ac
jchaasis <jchaasis@gmail.com>
2017-10-16 11:36:53 -0400

    Add file with sample data

---
+++ data.js
@@ -0,0 +1,50 @@
+let data = {
+
+  {event: {
+    description: "pickup soccer game",
+    category: "sports/outdoors",
+    start: "1:00 pm",
+    stop: "3:00pm",
+    location: {
+      lat:35.227306,
+      long: -80.848171,
+      }
+
+    }
+  },
+  {event: {
+    description: "Football for a cure.",
+    category: "Community",
+    start: "7:00 pm",
+    stop: "9:00 pm",
+    location: {
+      lat: 35.225111,
+      long: -80.851340,
+      },
+
+    }
+  },
+  {event: {
+    description: "Rock concert",
+    category: "Music/Art",
+    start: "10:00 pm",
+    stop: "12:00 am",
+    location: {
+      lat: 35.224705,
+      long: -80.839764,
+      }
+
+    }
+  },
+  {event: {
+    description: "Brunch fest",
+    category: "Food/Bev",
+    start: "11:00 am",
+    stop: "2:00 pm",
+    location: {
+      lat: 35.225412,
+      long: -80.842691,
+      }
+    }
+  },
+}
