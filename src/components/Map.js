c17defbf1271c89874f0d3b323ec2f08f249c3bd
Antonio Chambers <r.antonio.chambers@gmail.com>
2017-10-25 11:58:51 -0400

    changed icons

--- src/components/Map.js
+++ src/components/Map.js
@@ -79,10 +79,14 @@ plotPoints(){
 
     //create a div for the marker
     let el = document.createElement('div');
-    el.classList.add('shine');
+    // el.classList.add('shine');
     el.classList.add('eventMarker');
 
     let newMark = this.props.events[i] //shortened for use for below
+
+    if (newMark.likes.length >= 20) {
+      el.classList.add('shine')
+    }
 
     //give the events a marker that matches their category
     if (newMark.category === "Sports/Outdoors") {
@@ -95,7 +99,7 @@ plotPoints(){
       el.classList.add('marker2')
     } else if (newMark.category === "") {
       el.classList.add('marker5')
-    }
+    } 
 
     // create a custom popup for each individual item
     let popup = new window.mapboxgl.Popup({ offset: 25 })
