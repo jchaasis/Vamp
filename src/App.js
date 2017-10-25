8b8a79cfdcdf3be60d7a8e0bee1be7933348b468
jchaasis <jchaasis@gmail.com>
2017-10-25 15:39:04 -0400

    Move sortTime function to util.js file.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import './styles/App.css';
 import { connect } from 'react-redux'
 
 //import actions
-import { displayEvents, getCurrentLoc, sortTime } from './actions';
+import { displayEvents, getCurrentLoc } from './actions';
 
+//import random functions
+import { sortTime } from './util'
 //import components
 import EventForm from './components/EventForm';
 import NavBar from './components/NavBar';
@@ -37,44 +39,6 @@ class App extends Component {
     })
   }
 
-
-  //get the current time. Once we have that, compare the current time with the end time.
-  // sortTime(events){
-  //   console.log(events)
-  //
-  //   //array to store the events that are availableEvents
-  //   const sortedEvents = [];
-  //
-  //   //get the current time
-  //   let d = new Date()
-  //
-  //   //store the current hr and minute to use for comparison
-  //   let currHr = d.getHours()
-  //   let currMin = d.getMinutes()
-  //   console.log(currHr, currMin)
-  //
-  //   //iterate through the events endtimes.
-  //   for (let i = 0; i < events.length; i++){
-  //     // if (events[i].endTime)
-  //     //split the time into an array so that we can compare the event hrs and the event minutes with the current hrs and current minutes
-  //     let splitTimes = events[i].eventEnd.split(':');
-  //
-  //     //shortened for comparison
-  //     let eventEndHr = splitTimes[0];
-  //     let eventEndMin = splitTimes[1];
-  //
-  //     //if the event end hr is greater than the current hr, it is an event that should show up on the map so push it to the events. If the event end hr is equal to the current hr and the event end min is greater than the current minute, it should also show up on the map.
-  //     if (eventEndHr > currHr ){
-  //       sortedEvents.push(events[i])
-  //     } else if (eventEndHr == currHr && eventEndMin > currMin){
-  //       sortedEvents.push(events[i])
-  //     }
-  //   }
-  //
-  //   console.log(sortedEvents)
-  //   return(sortedEvents)
-  //   // let availableEvents = events.filter(event)
-  // }
 
   render() {
     //when the "Add event button is clicked, show the event form". pass the toggleEventForm function down to the form so that the exit and add buttons can use it to close the form.
